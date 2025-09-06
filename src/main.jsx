4e26f7c2638c93f62f8b3c400683b0bd17cf165a
JasphinVijayJ <jasphinvijayj@gmail.com>
2025-09-06 12:39:04 +0530

    Trying to Solve Path Problem

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/Pet-Adoption-React'>
+    <BrowserRouter basename='/Pet-Adoption-React/'>
       <App />
     </BrowserRouter>
   </StrictMode>,
